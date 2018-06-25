c3f861bba6026168b488426402cefda5d1e2110f
Romain CADART <romain.cadart@gmail.com>
2018-06-25 11:41:02 +0200

    clieyuazvgfizegy

---
+++ ProxiBanqueRSv1.0/src/domaine/ClientParticulier.java
@@ -0,0 +1,119 @@
+package domaine;
+
+public class ClientParticulier extends Client {
+
+	private String numeroSecu;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private int codePostal;
+	private String ville;
+	private String telephone;
+	
+	
+	private Placement placement;
+	private CompteCourant compteCourant;
+	private CompteEpargne compteEpargne;
+	
+	
+	// CONSTRUCTEUR
+	
+	public ClientParticulier(String numeroSecu, String nom, String prenom, String adresse, int codePostal, String ville, String telephone, int idClient, double solde) {
+		super( idClient, solde);
+		this.numeroSecu=numeroSecu; this.nom= nom; this.prenom = prenom; this.adresse =adresse; this.codePostal= codePostal; this.ville =ville; this.telephone = telephone;
+	}
+	public ClientParticulier() {
+		super( 777, 777);
+		this.numeroSecu="777"; this.nom= "Raspoutine"; this.prenom = "charmant"; this.adresse ="Siberie"; this.codePostal= 99999; this.ville ="Kruchnikov"; this.telephone = "RaRaRaspoutineLoverOfTheRussianQueen";
+	}
+// ASCESSEURS
+	public String getNumeroSecu() {
+		return numeroSecu;
+	}
+
+
+	public void setNumeroSecu(String numeroSecu) {
+		this.numeroSecu = numeroSecu;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public Placement getPlacement() {
+		return placement;
+	}
+	public void setPlacement(Placement placement) {
+		this.placement = placement;
+	}
+	public CompteCourant getCompteCourant() {
+		return compteCourant;
+	}
+	public void setCompteCourant(CompteCourant compteCourant) {
+		this.compteCourant = compteCourant;
+	}
+	public CompteEpargne getCompteEpargne() {
+		return compteEpargne;
+	}
+	public void setCompteEpargne(CompteEpargne compteEpargne) {
+		this.compteEpargne = compteEpargne;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+
+	public int getCodePostal() {
+		return codePostal;
+	}
+
+
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	
+	
+}
